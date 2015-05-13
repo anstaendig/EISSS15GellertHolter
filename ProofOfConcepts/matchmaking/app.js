931a6f92846c1346f5532840a04f7134a329c42e
anstaendig <marcel.holter@gmail.com>
2015-05-13 10:44:43 +0200

    matchmaking poc: partial string search verbessert

--- ProofOfConcepts/matchmaking/app.js
+++ ProofOfConcepts/matchmaking/app.js
@@ -14,23 +14,17 @@ var db = mongo.db('mongodb://localhost:27017/test', {native_parser:true});
 db.bind('sick');
 
 // define parts for aggregation pipeline
-var a = {'$match': {'$or' : [ {'krankheiten' : 'Stinken'},{'krankheiten' : 'Grippe'}]}};
-
-var b = {'$project': 'krankheiten'};
-
+var a = {'$match': {'$or': [{'krankheiten': 'Stinken'},{'krankheiten': 'Grippe'}]}};
+//var b = {'$project': 'krankheiten'};
 var c = {'$unwind': '$krankheiten'};
-
 var d = {'$project': {'c': {'$concat':
-                            [{'$cond':[ {'$eq' : ['$krankheiten', 'Stinken' ]},'Stinken','']},
-                            {'$cond':[ {'$eq' : ['$krankheiten', 'Grippe']},'Grippe','']}]}}};
-
+                            [{'$cond':[{'$eq' : ['$krankheiten', 'Stinken' ]},'Stinken','']},
+                            {'$cond':[{'$eq' : ['$krankheiten', 'Grippe']},'Grippe','']}]}}};
 var e = {'$group': {'c': {'$addToSet': '$c'}, '_id': '$_id'}};
-var f = {'$unwind': '$c'};
-var g = {'$group': {'c': {'$sum': 1}, '_id': '$_id'}};
-var h = {'$match': {'c': {'$gte': 3}}};
+var h = {'$match': {'c': {'$size': 3}}};
 var i = {'$project': {'_id': '$_id'}};
 
-db.collection('sick').aggregate([a,c,d,e,f,g,h,i], function(err, result) {
+db.collection('sick').aggregate([a,c,d,e,h,i], function(err, result) {
   console.log(result.length);
   for (var i = 0; i < result.length; i++) {
     db.collection('sick').findOne({_id:result[i]._id}, function(err, matched) {
